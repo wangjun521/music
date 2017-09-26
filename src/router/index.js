fd7a7c59d4109b9bb0652e2672f3d1c174b0ab73
wangjun521 <wangjun20101120@163.com>
2017-09-26 16:02:40 +0800

    组件异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Rank from 'components/rank/rank'
-import Singer from 'components/singer/singer'
-import Search from 'components/search/search'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
+// import Recommend from 'components/recommend/recommend'
+// import Rank from 'components/rank/rank'
+// import Singer from 'components/singer/singer'
+// import Search from 'components/search/search'
+// import SingerDetail from 'components/singer-detail/singer-detail'
+// import Disc from 'components/disc/disc'
+// import TopList from 'components/top-list/top-list'
+// import UserCenter from 'components/user-center/user-center'
 Vue.use(Router)
 
 export default new Router({
@@ -15,32 +15,76 @@ export default new Router({
     { path: '/', redirect: 'recommend', name: '' },
     { path: '/recommend',
       name: '',
-      component: Recommend,
+      component: (resolve) => {
+        import('components/recommend/recommend').then((module) => {
+          resolve(module)
+        })
+      },
       children: [
-        {path: ':id', component: Disc}
+        {path: ':id',
+          component: (resolve) => {
+            import('components/disc/disc').then((module) => {
+              resolve(module)
+            })
+          }}
       ]
     },
     { path: '/rank',
       name: '',
-      component: Rank,
+      component: (resolve) => {
+        import('components/rank/rank').then((module) => {
+          resolve(module)
+        })
+      },
       children: [
-        {path: ':id', component: TopList}
+        {path: ':id',
+          component: (resolve) => {
+            import('components/top-list/top-list').then((module) => {
+              resolve(module)
+            })
+          }
+        }
       ]
     },
     { path: '/singer',
       name: '',
-      component: Singer,
+      component: (resolve) => {
+        import('components/singer/singer').then((module) => {
+          resolve(module)
+        })
+      },
       children: [
-      { path: ':id', name: '', component: SingerDetail }
+        { path: ':id',
+          name: '',
+          component: (resolve) => {
+            import('components/singer-detail/singer-detail').then((module) => {
+              resolve(module)
+            })
+          } }
       ]
     },
     { path: '/search',
       name: '',
-      component: Search,
+      component: (resolve) => {
+        import('components/search/search').then((module) => {
+          resolve(module)
+        })
+      },
       children: [
-      { path: ':id', name: '', component: SingerDetail }
+        { path: ':id',
+          name: '',
+          component: (resolve) => {
+            import('components/singer-detail/singer-detail').then((module) => {
+              resolve(module)
+            })
+          } }
       ]
     },
-    {path: '/user', component: UserCenter}
+    {path: '/user',
+      component: (resolve) => {
+        import('components/user-center/user-center').then((module) => {
+          resolve(module)
+        })
+      }}
   ]
 })
